fcbe3518b24a58837137c66304c821911694bcb6
appu12468 <123217932+appu12468@users.noreply.github.com>
2026-07-03 15:44:23 -0400

    Update main.dart

--- task_reminder/lib/main.dart
+++ task_reminder/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const TaskReminderApp());
 }
-
+//test change
 class TaskReminderApp extends StatelessWidget {
   const TaskReminderApp({super.key});
 
